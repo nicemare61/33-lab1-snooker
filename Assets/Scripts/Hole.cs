89429061ef06e472818190f1b769ecaa9fdf2859
nicemare61 <kittipoj.meer@bumail.net>
2023-11-28 22:04:15 +0700

    PlusScore

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    //                            int point
+    private void OnTriggerEnter(Collider other)
+    {
+       Ball b = other.gameObject.GetComponent<Ball>();
+
+       if (b != null) //ถ้า B มีอยู่จริง
+       {
+           GameManager.instance.PlayerScore += b.Point; //+ b.point เข้าไปใน PlayerScore
+           Destroy(b.gameObject);
+       }
+    }
+}
