53bf3bea2c8f35b94ceda047f3574485eed6c963
nicemare61 <kittipoj.meer@bumail.net>
2023-11-28 21:02:51 +0700

    cueball and Rotate

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,27 +9,31 @@ public class GameManager : MonoBehaviour
     [SerializeField ]private int playerScore;
     [SerializeField] private GameObject ballPrefab;
     [SerializeField] private GameObject[] ballPositions;
+
+    [SerializeField] private GameObject cueBall;
+    [SerializeField] private GameObject ballLine;
+
+    [SerializeField] private float xInput;
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
         
         //set balls on the table
-        SetBalls(BallColors.White, 0);
-        SetBalls(BallColors.Red,1);
-        SetBalls(BallColors.Yellow,2);
-        SetBalls(BallColors.Green,3);
-        SetBalls(BallColors.Brown,4);
-        SetBalls(BallColors.Blue,5);
-        SetBalls(BallColors.Pink,6);
-        SetBalls(BallColors.Black,7);
+        SetBalls(BallColors.Red,0);
+        SetBalls(BallColors.Yellow,1);
+        SetBalls(BallColors.Green,2);
+        SetBalls(BallColors.Brown,3);
+        SetBalls(BallColors.Blue,4);
+        SetBalls(BallColors.Pink,5);
+        SetBalls(BallColors.Black,6);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RotateBall();
     }
 
     void SetBalls(BallColors color, int pos)
@@ -37,5 +41,11 @@ public class GameManager : MonoBehaviour
         GameObject ball = Instantiate(ballPrefab, ballPositions[pos].transform.position, Quaternion.identity);
         Ball b = ball.GetComponent<Ball>();
         b.SetColorsAndPoints(color);
+    }
+
+    void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cueBall.transform.Rotate(new Vector3(0f,xInput/10,0f));
     }
 }
